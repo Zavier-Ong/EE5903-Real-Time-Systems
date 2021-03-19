dd422618cb49b7ad376eeacf604155feffca1d98
Zavier-Ong <zacohjayjay@gmail.com>
2021-03-19 17:39:29 +0800

    continue edf

--- CA2/edf.py
+++ CA2/edf.py
@@ -9,18 +9,21 @@ from operator import attrgetter
 
 class Task:
     def __init__(self, details, idx):
-        self.completed = False;
-        self.progress = 0;
-        self.job_start = 0;
+        self.completed = False
+        self.progress = 0
+        self.job_start = -1
+        self.job_completed = 0
+        self.next_allowed_task = 0
+        self.ready = True
         
         split = details.split(',')
-        self.task_id = idx
+        self.tid = idx
         self.wcet = int(split[0])
         self.next_deadline = int(split[1])
         self.period = int(split[2])
     
     def print_details(self):
-        print('Task ID: ' + str(self.task_id) + ' WCET: ' + str(self.wcet) + 
+        print('Task ID: ' + str(self.tid) + ' WCET: ' + str(self.wcet) + 
               ' Deadline: ' + str(self.next_deadline) + 
               ' Period: ' + str(self.period))
 
@@ -37,20 +40,58 @@ def utilization_test(task_set):
     else:
         return False
     
-def get_earliest_deadline_task():
-    return min(task_set, key = attrgetter('next_deadline'))
+def get_earliest_deadline_task_list():
+    return sorted(task_set, key = attrgetter('next_deadline'))
 
 class Scheduler:
     def __init__(self):
-        self.running_tasks = set()
-        self.curr_task = -1
+        self.curr_tid = -1;
+        self.is_busy = False;
         
-    def update_task(self, t):
-        # updating all currently running tasks
-        for task in self.running_tasks:
+    def get_task_from_id(self, tid):
+        for task in task_set:
+            if task.tid == tid:
+                return task
+        return None
+    
+    def update_tasks(self, t):
+        #check which task has the earliest deadline
+        task_list = get_earliest_deadline_task_list()
         
+       # deadlines = []
+       # for task in task_list:
+       #    deadlines.append(task.next_deadline)
+       # print('{} {} {}'.format(deadlines[0], deadlines[1], deadlines[2]))
         
-        
+        #list of tasks ordered based on their earliest deadlines
+        for task in task_list:
+            if not task.completed:
+                if not self.is_busy:
+                    if t >= task.next_allowed_task:
+                        if task.job_start == -1:
+                            task.job_start = t
+                            self.curr_tid = task.tid
+                            self.is_busy = True;
+                            print('Task {} has started at T={}'.format(task.tid, t))
+                    
+                if task.progress >= task.wcet:
+                    task.completed = True
+                    self.curr_tid = -1
+                    self.is_busy = False;
+                    print('Task {} completed execution from T={} to T={}'.format(task.tid, task.job_start, t))
+                    
+                if self.curr_tid == task.tid:
+                    task.progress += 1
+                
+            if (task.next_deadline == t):
+                task.completed = False
+                task.progress = 0
+                task.job_start = -1
+                task.next_deadline += task.period
+                task.next_allowed_task += task.period
+                print('Task {} has reached its deadline of T={}. The new deadline is T={}'.format(task.tid, (task.next_deadline-task.period), task.next_deadline))
+  
+    
     def run(self, duration):
         t = 0;
         while t < duration:
@@ -74,5 +115,5 @@ utilization_test(task_set)
    
 #start scheduling
 scheduler = Scheduler()
-scheduler.run(hyper_period*3)
+scheduler.run(hyper_period)
 

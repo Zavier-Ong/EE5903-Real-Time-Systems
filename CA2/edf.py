85da4c5645eac9c9cc4aed152b543ffd73c8d16c
Zavier-Ong <zacohjayjay@gmail.com>
2021-03-23 18:21:26 +0800

    edit edf

--- CA2/edf.py
+++ CA2/edf.py
@@ -13,6 +13,7 @@ class Task:
         self.progress = 0
         self.job_start = -1
         self.num_jobs_completed = 0
+        self.num_jobs_missed = 0
         self.next_allowed_task = 0
         self.ready = True
         self.num_preemptions = 0
@@ -47,7 +48,7 @@ def get_earliest_deadline_task_list(task_list):
 
 class Scheduler:
     def __init__(self):
-        self.curr_tid = -1;
+        self.curr_tid = -1
         self.tasks_completed = 0
         self.tasks_missed = 0
         
@@ -59,90 +60,22 @@ class Scheduler:
 
     def schedule(self, t):
         for task in task_set:
-            # task enters ready state once it has exceeded its period
-            if t == task.next_allowed_task:
-                task.ready = True
-        
-        task_list = [task for task in task_set if task.ready]
-        
-        #sort ready tasks
-        task_list = get_earliest_deadline_task_list(task_list)
-        tid = []
-        for task in task_list:
-           tid.append(task.tid)
-        #print('>>>>')
-        #print('T={} {}'.format(t, tid))
-    
-        self.update_tasks(task_list, t)
-        
-        for task in task_set:
+            if task.tid == self.curr_tid:
+                task.progress += 1
+                if task.progress == task.wcet:
+                    task.completed = True
+            
             if task.next_deadline == t:
                 if not task.completed:
-                    print('Task {} deadline missed.'.format(task.tid))
-                    self.tasks_missed += 1
-                task.completed = False
+                    print('Deadline for task {} is missed at T='.format(task.tid, t))
                 task.next_deadline += task.period
                 task.progress = 0
-                print('Task {} has reached its deadline of T={}. The new deadline is T={}'.format(task.tid, (task.next_deadline-task.period), task.next_deadline))
+                task.completed = False
         
-        #print('----')
+        task_list = get_earliest_deadline_task_list(task_set)
         
-    def update_tasks(self, task_list, t):
-            #scheduler is idle if task_list is empty
-        if not task_list:
-            print('Scheduler is idle at T={}'.format(t))
-            self.curr_tid = -1
-            return
-        # starting case
-        if self.curr_tid == -1:
-            #print('Task {} start at T={}'.format(task_list[0].tid, t))
-            task_list[0].job_start = t
-        elif self.curr_tid != task_list[0].tid:
-            print('Task {} preempted Task {} at T={}'.format(task_list[0].tid, self.curr_tid, t))
-            preempted_task = self.get_task_from_id(self.curr_tid)
-            preempted_task.num_preemptions += 1
-            if task_list[0].job_start == -1:
-                task_list[0].job_start = t
-                print('Task {} started at T={}.'.format(task_list[0].tid, t))
-            else:
-                print('Task {} resuming execution at T={}.'.format(task_list[0].tid, t)) 
-        
-        self.curr_tid = task_list[0].tid
-        completed_task_flag = False
-        #add progress to earliest deadline task
-        task_list[0].progress += 1
-          
-        #print('Task {} progress: {} wcet: {}'.format(task_list[0].tid, task_list[0].progress, task_list[0].wcet))
-        if task_list[0].progress == task_list[0].wcet:
-            self.tasks_completed += 1
-            if task_list[0].num_preemptions == 0:
-                print('Task {} completed execution from T={} to T={}.'.format(task_list[0].tid, task_list[0].job_start, t))
-            else:
-                print('Task {} completed execution from T={} to T={} with {} preemption(s).'.format(task_list[0].tid, task_list[0].job_start, t, task_list[0].num_preemptions))
-            task_list[0].completed = True
-            task_list[0].ready = False
-            task_list[0].num_jobs_completed += 1
-            task_list[0].job_start = -1
-            task_list[0].next_allowed_task += task_list[0].period
-            task_list[0].num_preemptions = 0
-            completed_task_flag = True
-            self.curr_tid = -1
-        
-        # start second job
-        if completed_task_flag:
-            if len(task_list) >1:
-                self.curr_tid = task_list[1].tid
-                task_list[1].progress +=1
-                
-                if task_list[1].job_start == -1:
-                    task_list[1].job_start = t
-                    print('Task {} started at T={}.'.format(task_list[1].tid, t))
-                else:
-                    print('Task {} resuming execution at T={}.'.format(task_list[1].tid, t))
-            else:
-                self.curr_tid = -1
-                print('Scheduler is idle at T={}'.format(t))
-                return
+        if self.curr_tid == task_list[0].tid:
+            print('Task {} ran from T={} to T={}'.format(task.tid, task.job_start, )
         
     
     def run(self, duration):
@@ -158,7 +91,7 @@ class Scheduler:
 task_set = []
 f = open('taskset.txt', 'r')
 num_tasks = f.readline()
-hyper_period = 1;
+hyper_period = 1
 for i in range(int(num_tasks)):
     task = Task(f.readline().strip(), i+1)
     task.print_details()
